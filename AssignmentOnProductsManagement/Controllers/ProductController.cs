5a2eb8d34d7269958ec80fd893bf0e7f6c951fa1
Ujjwal Srivastava <ujjwal.srivastava.mst15@itbhu.ac.in>
2020-07-10 00:43:38 +0530

    Assign1

---
+++ AssignmentOnProductsManagement/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AssignmentOnProductsManagement.Models;
+
+namespace AssignmentOnProductsManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Products> li = new List<Products>()
+        {
+            new Products() { Pid=110001,Pname="Headphones",Price=299,Stock=10 }
+        };
+        public IActionResult Index()
+        {
+            return View(li);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Products p)
+        {
+            if (ModelState.IsValid)
+            {
+                li.Add(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Create");
+            }
+        }
+    }
+}
---
+++ AssignmentOnProductsManagement/Models/Products.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AssignmentOnProductsManagement.Models
+{
+    public class Products
+    {
+        [Required(ErrorMessage = "ID Required")]
+        public int Pid { get; set; }
+        [Required(ErrorMessage = "Name Required")]
+        public string Pname { get; set; }
+        [Required(ErrorMessage = "Invalid")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Invalid")]
+        public int Stock { get; set; }
+
+
+    }
+}
